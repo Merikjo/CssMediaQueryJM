04c0c1fafc5793e40ba64278bb6143614a4e24b9
Johanna Merikanto <johanna.merikanto@stud.pointcollege.fi>
2016-04-07 10:26:18 +0300

    MVC-projektin luonti.

---
+++ CssMediaQueryJM/CssMediaQueryJM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CssMediaQueryJM.MvcApplication" Language="C#" %>
---
+++ CssMediaQueryJM/CssMediaQueryJM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CssMediaQueryJM.Startup))]
+namespace CssMediaQueryJM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
